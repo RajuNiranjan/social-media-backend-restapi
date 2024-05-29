5e30e8b426abfeabcee606afff8d7a306e873732
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-05-29 23:23:17 +0530

    added basic server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import express, { json } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+
+const PORT = 5000;
+
+app.use("/api", (req, res, next) => {
+  res.send({ message: "Hello world" });
+});
+
+app.listen(PORT, () =>
+  console.log(`server is running in port number : ${PORT}`)
+);
