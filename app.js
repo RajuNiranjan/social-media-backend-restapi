21bbd3c7c5be60a302a6c0b4fb29d02787e226d6
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-05-30 22:14:20 +0530

    connected to database

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ import express, { json } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+dotenv.config();
+
 const app = express();
 
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
-app.use("/api", (req, res, next) => {
-  res.send({ message: "Hello world" });
-});
+const dbUrl = process.env.MONGO_DB;
+
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("server connected to database"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () =>
-  console.log(`server is running in port number : ${PORT}`)
+  console.log(`server is running at port number : ${PORT}`)
 );
