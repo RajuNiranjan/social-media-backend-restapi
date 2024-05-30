e0ee6971ac4aaaed398e658a24eafb745fdadeef
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-05-30 22:19:42 +0530

    created user schema

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 5,
+  },
+});
+
+export default mongoose.model("User", UserSchema)
+
